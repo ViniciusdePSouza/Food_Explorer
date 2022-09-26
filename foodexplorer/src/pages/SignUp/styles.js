dea73c1f886848dd48d8674dd965857b83fe8b20
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-09-26 10:03:02 -0300

    Responsive layout of the SignIn and SignOut pages added

--- foodexplorer/src/pages/SignUp/styles.js
+++ foodexplorer/src/pages/SignUp/styles.js
@@ -8,7 +8,11 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 0 10.8rem
+    padding: 0 10.8rem;
+
+    @media (max-width: 920px) {
+        padding: 0 5rem;
+    }
 `
 
 export const Content = styled.div`
@@ -40,11 +44,55 @@ export const Content = styled.div`
 
             text-align: center;
         }
+
+        @media (max-width: 1200px) {
+            gap: 0;
+
+            >img {
+                width: 4rem;
+                height: 4rem;
+            }
+
+            >h1 {
+                margin: 0;
+                padding: 0;
+                
+                margin: 0 1rem 0 0.2rem;
+
+                font-size: 3rem;
+                line-height: 3.5rem;
+            }
+        } 
+
+        @media (max-width: 920px) {
+            display: flex;
+            align-items: center;
+            text-align: center;
+            justify-content: center;
+            flex-direction: column;
+
+            >h1 {
+                margin-top: 1rem;
+            }
+        }
+    }
+
+    @media (max-width: 920px) {
+        flex-direction: column;
+
+        margin: -2.5rem 0;
+
+        gap: 3.2rem;
+
+        display: flex;
+        align-items: center;
+        text-align: center;
+        justify-content: center;
     }
 `
 
 export const Form = styled.form`
-    width: 40%;
+    width: 50%;
     
     display: flex;
     flex-direction: column;
@@ -61,5 +109,19 @@ export const Form = styled.form`
         font-weight: 500;
         font-size: 3.2rem;
         line-height: 2.4rem;
+    }
+
+    @media (max-width: 1200px) {
+        width: 70%;
+
+        margin: 0;
+    }
+
+    @media (max-width: 920px) {
+        width: 100%;
+
+        > h1 {
+            font-size: 2rem;
+        }
     }
 `
