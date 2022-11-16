06b9577656c12d285bafd08d69f0928c75fc90a1
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-11-16 20:40:29 -0300

    Fetching data already

--- foodexplorer/src/Components/Card/index.jsx
+++ foodexplorer/src/Components/Card/index.jsx
@@ -1,22 +1,29 @@
 import { Container, FavIcon, MinusIcon, PlusIcon } from './styles'
 
 import { FiHeart, FiMinus, FiPlus } from 'react-icons/fi'
-import parmaToast from '../../assets/dishes/parma-toast.png'
 
 import { useNavigate } from 'react-router-dom'
+import  {useState, useEffect } from 'react'
 
 import { Button } from '../../Components/Button'
 
 export function Card({ data, ...rest }) {
     const navigate = useNavigate()
 
+    // const [photo, setPhoto] = useState('')
+
     function handleDetails() {
         navigate(`/details/${data.id}`)
     }
 
+    // useEffect(() => {
+    //     const IMG = URL.createObjectURL(data.photo)
+    //     console.log(IMG)
+    //   }, [])
+
     return (
         <Container onClick={handleDetails}>
-            <img src={parmaToast} alt="" />
+            <img src={data.photo} alt="" />
 
             <FavIcon>
                 <FiHeart/>
--- foodexplorer/src/pages/Home/index.jsx
+++ foodexplorer/src/pages/Home/index.jsx
@@ -14,24 +14,38 @@ import { Input } from '../../Components/Input'
 
 import banner from '../../assets/banner.svg'
 
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 import { api } from '../../services/api'
 
 export function Home() {
   const [search, setSearch] = useState('')
   const [mainDishes, setMainDishes] = useState([])
+  const [drinks, setDrinks] = useState([])
+  const [desserts, setDesserts] = useState([])
 
   useEffect(() => {
     async function fetchMainDishes() {
-      const response  = await api.get(`/getters/main`)
-      setMainDishes(response)
+      const response = await api.get(`/getters/main`)
+      setMainDishes(response.data)
+
+    }
+    async function fetchDrinks() {
+      const response = await api.get(`/getters/drink`)
+      setDrinks(response.data)
+
+    }
+    async function fetchDesserts() {
+      const response = await api.get(`/getters/dessert`)
+      setDesserts(response.data)
+
     }
 
     fetchMainDishes()
-
-    console.log(mainDishes)
+    fetchDesserts()
+    fetchDrinks()
   }, [])
+
 
   return (
     <Container>
@@ -50,51 +64,44 @@ export function Home() {
         </Banner>
 
         <div className="div-input">
-          <Input icon={FiSearch} placeholder="Pesquisar prato nos favoritos" type="text" onChange={e => setSearchValue(e.target.value)}/>
+          <Input icon={FiSearch} placeholder="Pesquisar prato nos favoritos" type="text" onChange={e => setSearchValue(e.target.value)} />
         </div>
 
         <h1>Pratos Principais</h1>
-        
-        <Carousel cols={4} rows={1} loop={true} gap={0}>
-          <Carousel.Item>
-          {
-            <Card data={{
-              name: 'teste',
-              price:'22,50',
-              description: 'teste de descrição',
-              id:5
-            }}/>
-          }
-          </Carousel.Item>
 
+        <Carousel cols={3} rows={1} loop={true} gap={0}>
+          {
+            mainDishes.map(dish => (
+              <Carousel.Item>
+                <Card data={dish}
+                 />
+              </Carousel.Item>
+            ))
+          }
         </Carousel>
 
         <h1>Sobremesas</h1>
 
-        <Carousel cols={4} rows={1} loop={true} gap={0}>
-          <Carousel.Item>
-          <Card data={{
-              name: 'teste',
-              price:'22,50',
-              description: 'teste de descrição',
-              id:5
-            }}/>
-          </Carousel.Item>
-
+        <Carousel cols={3} rows={1} loop={true} gap={0}>
+          {
+            desserts.map(dessert => (
+              <Carousel.Item>
+                <Card data={dessert} />
+              </Carousel.Item>
+            ))
+          }
         </Carousel>
 
         <h1>Bebidas</h1>
 
-        <Carousel cols={4} rows={1} loop={true} gap={0}>
-          <Carousel.Item>
-            <Card data={{
-              name: 'teste',
-              price:'22,50',
-              description: 'teste de descrição',
-              id:5
-            }}/>
-          </Carousel.Item>
-
+        <Carousel cols={3} rows={1} loop={true} gap={0}>
+          {
+            drinks.map(drink => (
+              <Carousel.Item>
+                <Card data={drink} />
+              </Carousel.Item>
+            ))
+          }
         </Carousel>
       </Content>
 
